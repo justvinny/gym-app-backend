d115bd3edfc8afdd2ccb6d593b19cdf3d454e4cd
Vinson Beduya <vinsonemb.1551994@gmail.com>
2022-02-16 20:01:36 +1300

    Added FirebaseAdmin and Serilog

---
+++ gym-app-backend/Extensions/FirebaseExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+namespace gym_app_backend.Extensions
+{
+    public static class FirebaseExtensions
+    {
+        public static void AddFirebaseAdmin(this IServiceCollection service, IConfiguration config)
+        {
+            try
+            {
+                var privateKeyPath = config["Firebase:PrivateKey"];
+                FirebaseApp.Create(new AppOptions
+                {
+                    Credential = GoogleCredential.FromFile(privateKeyPath)
+                });
+                Console.WriteLine("Successfully Added Firebase Admin.");
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"AddFirebaseAdmin: {ex.Message}");
+                throw;
+            }
+
+        }
+    }
+}
--- gym-app-backend/Program.cs
+++ gym-app-backend/Program.cs
@@ -1,25 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
+using gym_app_backend.Extensions;
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up Web API...");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddFirebaseAdmin(builder.Configuration);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.UseSerilogRequestLogging();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Error($"Program.cs Error: {ex.Message}");
+}
+finally
+{
+    Log.Information("Shutdown complete.");
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
